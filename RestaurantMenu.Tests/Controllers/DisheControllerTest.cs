b511f47349d57ddcd686ff5e74a7d71d78f3fd52
GalanovaM <mariaygalanova.1999@yandex.ru>
2022-01-23 23:30:52 +0300

    add controllers tests

---
+++ RestaurantMenu.Tests/Controllers/DisheControllerTest.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RestaurantMenu.Controllers;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Timetable.Infrastructure.Models.Service.Dishe;
+using Xunit;
+
+namespace Timetable.Tests.Controllers
+{
+    /// <summary>
+    ///     Dishe controller test
+    /// </summary>
+    public class DisheControllerTest
+    {
+        Mock<IDisheService> disheServiceMock;
+
+        public DisheControllerTest()
+        {
+            disheServiceMock = new Mock<IDisheService>();
+        }
+
+        [Fact]
+        public async Task GetDishes_ShouldReturn_Dishes()
+        {
+            //arrange
+            List<DisheDto> dishes = GetTestDishes();
+
+            disheServiceMock.Setup(r => r.GetDishesAsync()).Returns(Task.FromResult(dishes));
+
+            DisheController controller = new DisheController(disheServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.GetDishes();
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetDisheById_ShouldReturn_Dishe()
+        {
+            //arrange
+            var dishes = GetTestDishes();
+
+            disheServiceMock.Setup(r => r.GetDisheByIdAsync(dishes[0].Id)).Returns(Task.FromResult(dishes[0]));
+
+            DisheController controller = new DisheController(disheServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.GetDishe(dishes[0].Id);
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddDishe_ShouldReturn_Dishe()
+        {
+            //arrange
+            DisheDto dishe = new DisheDto()
+            {
+                Id = 11,
+                Name = "Medovik",
+                Price = 700
+            };
+
+            disheServiceMock.Setup(r => r.CreateDisheAsync(dishe)).Returns(Task.FromResult(new CreateDisheResponseModel() {Dishe = dishe, Type = DisheResponseType.Success }));
+
+            DisheController controller = new DisheController(disheServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.Post(dishe);
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        private List<DisheDto> GetTestDishes()
+        {
+            List<DisheDto> dishes = new List<DisheDto>
+            {
+                new DisheDto
+                {
+                    Id = 1,
+                    Name = "Vareniki",
+                    Price = 100
+                },
+
+                new DisheDto
+                {
+                    Id = 2,
+                    Name = "Pasta",
+                    Price = 300
+                }
+            };
+
+            return dishes;
+        }
+    }
+}
---
+++ RestaurantMenu.Tests/Controllers/OrderControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RestaurantMenu.Controllers;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Timetable.Infrastructure.Models.Service.Order;
+using Xunit;
+
+namespace Timetable.Tests.Controllers
+{
+    /// <summary>
+    ///     Order controller test
+    /// </summary>
+    public class OrderControllerTest
+    {
+        Mock<IOrderService> orderServiceMock;
+
+        public OrderControllerTest()
+        {
+            orderServiceMock = new Mock<IOrderService>();
+        }
+
+        [Fact]
+        public async Task GetOrders_ShouldReturn_Orders()
+        {
+            //arrange
+            List<OrderDto> orders = GetTestOrders();
+
+            orderServiceMock.Setup(r => r.GetOrdersAsync()).Returns(Task.FromResult(orders));
+
+            OrderController controller = new OrderController(orderServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.GetOrders();
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetOrderById_ShouldReturn_Order()
+        {
+            //arrange
+            var orders = GetTestOrders();
+
+            orderServiceMock.Setup(r => r.GetOrderByIdAsync(orders[0].Id)).Returns(Task.FromResult(orders[0]));
+
+            OrderController controller = new OrderController(orderServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.GetOrder(orders[0].Id);
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddOrder_ShouldReturn_Order()
+        {
+            //arrange
+            OrderDto order = new OrderDto()
+            {
+                Id = 11,
+                Status = "Confirm"
+            };
+
+            orderServiceMock.Setup(r => r.CreateOrderAsync(order)).Returns(Task.FromResult(new CreateOrderResponseModel() {Order = order, Type = OrderResponseType.Success }));
+
+            OrderController controller = new OrderController(orderServiceMock.Object);
+
+            //act
+            IActionResult? result = await controller.Post(order);
+            OkObjectResult? okResult = result as OkObjectResult;
+
+            //assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        private List<OrderDto> GetTestOrders()
+        {
+            List<OrderDto> orders = new List<OrderDto>
+            {
+                new OrderDto
+                {
+                    Id = 1,
+                    Status = "Waiting"
+                },
+
+                new OrderDto
+                {
+                    Id = 2,
+                    Status = "Coocking"
+                }
+            };
+
+            return orders;
+        }
+    }
+}
