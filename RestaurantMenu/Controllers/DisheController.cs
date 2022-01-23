7c1139776dec9c20d5ba2ea42345cc2048c04475
GalanovaM <mariaygalanova.1999@yandex.ru>
2022-01-23 23:29:38 +0300

    add controllers

---
+++ RestaurantMenu/Controllers/DisheController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services.Interfaces;
+using Timetable.Infrastructure.Models.Service.Dishe;
+
+namespace RestaurantMenu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisheController : ControllerBase
+    {
+        private readonly IDisheService _disheService;
+
+        public DisheController(IDisheService disheService)
+        {
+            _disheService = disheService;
+        }
+
+        [HttpGet("GetDishes")]
+        public async Task<IActionResult> GetDishes()
+        {
+            List<DisheDto> dishes = await _disheService.GetDishesAsync();
+
+            return Ok(dishes);
+        }
+
+        [HttpGet("GetDishe/{id}")]
+        public async Task<IActionResult> GetDishe(int id)
+        {
+            DisheDto dishe = await _disheService.GetDisheByIdAsync(id);
+
+            return Ok(dishe);
+        }
+
+        [HttpGet("GetDisheByName/{name}")]
+        public async Task<IActionResult> GetDishe(string name)
+        {
+            DisheDto dishe = await _disheService.GetDisheByNameAsync(name);
+
+            return Ok(dishe);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(DisheDto dishe)
+        {
+            CreateDisheResponseModel createDisheResponse = await _disheService.CreateDisheAsync(dishe);
+
+            if (createDisheResponse.Type == DisheResponseType.Success)
+            {
+                return Ok(createDisheResponse);
+            }
+
+            return BadRequest(createDisheResponse);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, DisheDto dishe)
+        {
+            EditDisheResponseModel editDisheResponse = await _disheService.EditDisheAsync(id, dishe);
+
+            if (editDisheResponse.Type == DisheResponseType.Success)
+            {
+                return Ok(editDisheResponse);
+            }
+
+            return BadRequest(editDisheResponse);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            DisheResponseType disheResponse = await _disheService.DeleteDisheAsync(id);
+
+            if (disheResponse == DisheResponseType.Success)
+            {
+                return Ok(disheResponse);
+            }
+
+            return BadRequest(disheResponse);
+        }
+    }
+}
---
+++ RestaurantMenu/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services.Interfaces;
+using Timetable.Infrastructure.Models.Service.Order;
+
+namespace RestaurantMenu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("GetOrders")]
+        public async Task<IActionResult> GetOrders()
+        {
+            List<OrderDto> orders = await _orderService.GetOrdersAsync();
+
+            return Ok(orders);
+        }
+
+        [HttpGet("GetOrder/{id}")]
+        public async Task<IActionResult> GetOrder(int id)
+        {
+            OrderDto order = await _orderService.GetOrderByIdAsync(id);
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(OrderDto order)
+        {
+            CreateOrderResponseModel createOrderResponse = await _orderService.CreateOrderAsync(order);
+
+            if (createOrderResponse.Type == OrderResponseType.Success)
+            {
+                return Ok(createOrderResponse);
+            }
+
+            return BadRequest(createOrderResponse);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, OrderDto order)
+        {
+            EditOrderResponseModel editOrderResponse = await _orderService.EditOrderAsync(id, order);
+
+            if (editOrderResponse.Type == OrderResponseType.Success)
+            {
+                return Ok(editOrderResponse);
+            }
+
+            return BadRequest(editOrderResponse);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            OrderResponseType orderResponse = await _orderService.DeleteOrderAsync(id);
+
+            if (orderResponse == OrderResponseType.Success)
+            {
+                return Ok(orderResponse);
+            }
+
+            return BadRequest(orderResponse);
+        }
+    }
+}
