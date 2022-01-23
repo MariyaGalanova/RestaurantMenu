d989095024c3d27d20818510291a6cf39e11380a
GalanovaM <mariaygalanova.1999@yandex.ru>
2022-01-23 23:29:24 +0300

    register services and repositories

--- RestaurantMenu/Program.cs
+++ RestaurantMenu/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantMenu.Database;
+using RestaurantMenu.Database.Repositories;
+using RestaurantMenu.Database.Repositories.Interfaces;
+using RestaurantMenu.Infrastructure.Services;
+using RestaurantMenu.Infrastructure.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Register database context
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+
+//  Register services
+builder.Services.AddTransient<IDisheService, DisheService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+
+//  Register repositories
+builder.Services.AddTransient<IDisheRepository, DisheRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
