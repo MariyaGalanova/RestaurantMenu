c9681e6c66367ca9854c8fbd950512f9b097da4a
GalanovaM <mariaygalanova.1999@yandex.ru>
2022-01-23 23:30:25 +0300

    add repositories tests

---
+++ RestaurantMenu.Tests/Repositories/DisheRepositoryTest.cs
@@ -0,0 +1,145 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RestaurantMenu.Database;
+using RestaurantMenu.Database.Models;
+using RestaurantMenu.Database.Repositories;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Timetable.Tests.Repositories
+{
+    /// <summary>
+    ///     Dishe repository test
+    /// </summary>
+    public class DisheRepositoryTest
+    {
+        private readonly DbContextOptions<DatabaseContext>? options;
+        private readonly DatabaseContext context;
+
+        public DisheRepositoryTest()
+        {
+            options = new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase(databaseName: "TimetableDB").Options;
+            context = new DatabaseContext(options);
+        }
+
+        [Fact]
+        public async Task GetDishes_ShouldReturn_Dishes()
+        {
+            //arange
+            ClearDatabase(context);
+
+            var dishesNew = AddDb(context);
+
+            var disheRepository = new DisheRepository(context);
+
+            //act
+            var result = await disheRepository.GetDishesAsync();
+
+            //assert
+            dishesNew.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task GetDisheById_ShouldReturn_Dishe()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var dishesNew = AddDb(context);
+
+            var disheRepository = new DisheRepository(context);
+
+            //act
+            var result = await disheRepository.GetDisheByIdAsync(dishesNew[0].Id);
+
+            //assert
+            dishesNew[0].Should().BeEquivalentTo(result);
+        }
+
+
+        [Fact]
+        public async Task AddDishe_ShouldReturn_Dishe()
+        {
+            //arrange
+            var dishe = new Dishe
+            {
+                Id = 11,
+                Name = "Medovik",
+                Price = 700
+            };
+
+            var disheRepository = new DisheRepository(context);
+
+            //act
+            var result = await disheRepository.CreateDisheAsync(dishe);
+
+            //assert
+            dishe.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task EditDishe_ShouldReturn_Dishe()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var dishesNew = AddDb(context);
+            var disheRepository = new DisheRepository(context);
+
+            //act
+            var result = await disheRepository.EditDisheAsync(dishesNew[0]);
+
+            //assert
+            dishesNew[0].Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task DeleteDishe_ShouldReturn_Dishe()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var dishesNew = AddDb(context);
+            var disheRepository = new DisheRepository(context);
+
+            //act
+            await disheRepository.DeleteDisheAsync(dishesNew[1]);
+
+            //assert
+            Assert.True(context.Dishes.FirstOrDefault(t => t.Id == dishesNew[1].Id) == null);
+        }
+
+        private Dishe[] AddDb(DatabaseContext database)
+        {
+            var dishesNew = new[] {
+
+                new Dishe
+                {
+                    Id = 1,
+                    Name = "Vareniki",
+                    Price = 100
+                },
+
+                new Dishe
+                {
+                    Id = 2,
+                    Name = "Pasta",
+                    Price = 300
+                }
+            };
+
+            database.Dishes.AddRange(dishesNew);
+            database.SaveChanges();
+
+            return dishesNew;
+        }
+        private async void ClearDatabase(DatabaseContext context)
+        {
+            foreach (var entity in context.Dishes)
+                context.Dishes.Remove(entity);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ RestaurantMenu.Tests/Repositories/OrderRepositoryTest.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RestaurantMenu.Database;
+using RestaurantMenu.Database.Models;
+using RestaurantMenu.Database.Repositories;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Timetable.Tests.Repositories
+{
+    /// <summary>
+    ///     Order repository test
+    /// </summary>
+    public class OrderRepositoryTest
+    {
+        private readonly DbContextOptions<DatabaseContext>? options;
+        private readonly DatabaseContext context;
+
+        public OrderRepositoryTest()
+        {
+            options = new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase(databaseName: "TimetableDB").Options;
+            context = new DatabaseContext(options);
+        }
+
+        [Fact]
+        public async Task GetOrders_ShouldReturn_Orders()
+        {
+            //arange
+            ClearDatabase(context);
+
+            var ordersNew = AddDb(context);
+
+            var orderRepository = new OrderRepository(context);
+
+            //act
+            var result = await orderRepository.GetOrdersAsync();
+
+            //assert
+            ordersNew.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task GetOrderById_ShouldReturn_Order()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var ordersNew = AddDb(context);
+
+            var orderRepository = new OrderRepository(context);
+
+            //act
+            var result = await orderRepository.GetOrderByIdAsync(ordersNew[0].Id);
+
+            //assert
+            ordersNew[0].Should().BeEquivalentTo(result);
+        }
+
+
+        [Fact]
+        public async Task AddOrder_ShouldReturn_Order()
+        {
+            //arrange
+            var order = new Order
+            {
+                Id = 11,
+                Status = "Confirm"
+            };
+
+            var orderRepository = new OrderRepository(context);
+
+            //act
+            var result = await orderRepository.CreateOrderAsync(order);
+
+            //assert
+            order.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task EditOrder_ShouldReturn_Order()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var ordersNew = AddDb(context);
+            var orderRepository = new OrderRepository(context);
+
+            //act
+            var result = await orderRepository.EditOrderAsync(ordersNew[0]);
+
+            //assert
+            ordersNew[0].Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task DeleteOrder_ShouldReturn_Order()
+        {
+            //arrange
+            ClearDatabase(context);
+
+            var ordersNew = AddDb(context);
+            var orderRepository = new OrderRepository(context);
+
+            //act
+            await orderRepository.DeleteOrderAsync(ordersNew[1]);
+
+            //assert
+            Assert.True(context.Orders.FirstOrDefault(t => t.Id == ordersNew[1].Id) == null);
+        }
+
+        private Order[] AddDb(DatabaseContext database)
+        {
+            var ordersNew = new[] {
+
+                new Order
+                {
+                    Id = 1,
+                    Status = "Waiting"
+                },
+
+                new Order
+                {
+                    Id = 2,
+                    Status = "Coocking"
+                }
+            };
+
+            database.Orders.AddRange(ordersNew);
+            database.SaveChanges();
+
+            return ordersNew;
+        }
+        private async void ClearDatabase(DatabaseContext context)
+        {
+            foreach (var entity in context.Orders)
+                context.Orders.Remove(entity);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
