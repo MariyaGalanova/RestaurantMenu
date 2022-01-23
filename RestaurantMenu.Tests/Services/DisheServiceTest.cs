6b5294a3e07f2828324852d33c20389ffdd252f2
GalanovaM <mariaygalanova.1999@yandex.ru>
2022-01-23 23:30:40 +0300

    add services tests

---
+++ RestaurantMenu.Tests/Services/DisheServiceTest.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+using Mapster;
+using Moq;
+using RestaurantMenu.Database.Models;
+using RestaurantMenu.Database.Repositories.Interfaces;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Timetable.Infrastructure.Models.Service.Dishe;
+using Xunit;
+
+namespace Timetable.Tests.Services
+{
+    /// <summary>
+    ///     Dishe service test
+    /// </summary>
+    public class DisheServiceTest
+    {
+        Mock<IDisheRepository> disheRepositoryMock;
+
+        public DisheServiceTest()
+        {
+            disheRepositoryMock = new Mock<IDisheRepository>();
+        }
+
+        [Fact]
+        public async Task GetDishes_ShouldReturn_Dishes()
+        {
+            //arrange
+            var dishes = GetTestDishes();
+
+            disheRepositoryMock.Setup(r => r.GetDishesAsync()).Returns(Task.FromResult(dishes.Adapt<List<Dishe>>()));
+
+            DisheService service = new DisheService(disheRepositoryMock.Object);
+
+            //act
+            List<DisheDto> result = await service.GetDishesAsync();
+
+            //assert
+            dishes.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task GetDisheById_ShouldReturn_Dishe()
+        {
+            //arrange
+            var dishes = GetTestDishes();
+
+            disheRepositoryMock.Setup(r => r.GetDisheByIdAsync(dishes[0].Id)).Returns(Task.FromResult(dishes[0].Adapt<Dishe>()));
+
+            DisheService service = new DisheService(disheRepositoryMock.Object);
+
+            //act
+            DisheDto result = await service.GetDisheByIdAsync(dishes[0].Id);
+
+            //assert
+            dishes[0].Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task AddDishe_ShouldReturn_Dishe()
+        {
+            //arrange
+            DisheDto dishe = new DisheDto()
+            {
+                Id = 11,
+                Name = "Medovik",
+                Price = 700
+            };
+
+            disheRepositoryMock.Setup(r => r.CreateDisheAsync(dishe.Adapt<Dishe>())).Returns(Task.FromResult(dishe.Adapt<Dishe>()));
+
+            DisheService service = new DisheService(disheRepositoryMock.Object);
+
+            //act
+            CreateDisheResponseModel result = await service.CreateDisheAsync(dishe);
+
+            //assert
+            Assert.True(result.Type == DisheResponseType.Success);
+        }
+
+        private List<DisheDto> GetTestDishes()
+        {
+            List<DisheDto> dishes = new List<DisheDto>
+            {
+                new DisheDto
+                {
+                    Id = 1,
+                    Name = "Vareniki",
+                    Price = 100
+                },
+
+                new DisheDto
+                {
+                    Id = 2,
+                    Name = "Pasta",
+                    Price = 300
+                }
+            };
+
+            return dishes;
+        }
+    }
+}
---
+++ RestaurantMenu.Tests/Services/OrderServiceTest.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Mapster;
+using Moq;
+using RestaurantMenu.Database.Models;
+using RestaurantMenu.Database.Repositories.Interfaces;
+using RestaurantMenu.Infrastructure.Enums;
+using RestaurantMenu.Infrastructure.Models.Database;
+using RestaurantMenu.Infrastructure.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Timetable.Infrastructure.Models.Service.Order;
+using Xunit;
+
+namespace Timetable.Tests.Services
+{
+    /// <summary>
+    ///     Order service test
+    /// </summary>
+    public class OrderServiceTest
+    {
+        Mock<IOrderRepository> orderRepositoryMock;
+
+        public OrderServiceTest()
+        {
+            orderRepositoryMock = new Mock<IOrderRepository>();
+        }
+
+        [Fact]
+        public async Task GetOrders_ShouldReturn_Orders()
+        {
+            //arrange
+            var orders = GetTestOrders();
+
+            orderRepositoryMock.Setup(r => r.GetOrdersAsync()).Returns(Task.FromResult(orders.Adapt<List<Order>>()));
+
+            OrderService service = new OrderService(orderRepositoryMock.Object);
+
+            //act
+            List<OrderDto> result = await service.GetOrdersAsync();
+
+            //assert
+            orders.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task GetOrderById_ShouldReturn_Order()
+        {
+            //arrange
+            var orders = GetTestOrders();
+
+            orderRepositoryMock.Setup(r => r.GetOrderByIdAsync(orders[0].Id)).Returns(Task.FromResult(orders[0].Adapt<Order>()));
+
+            OrderService service = new OrderService(orderRepositoryMock.Object);
+
+            //act
+            OrderDto result = await service.GetOrderByIdAsync(orders[0].Id);
+
+            //assert
+            orders[0].Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public async Task AddOrder_ShouldReturn_Order()
+        {
+            //arrange
+            OrderDto order = new OrderDto()
+            {
+                Id = 11,
+                Status = "Confirm"
+            };
+
+            orderRepositoryMock.Setup(r => r.CreateOrderAsync(order.Adapt<Order>())).Returns(Task.FromResult(order.Adapt<Order>()));
+
+            OrderService service = new OrderService(orderRepositoryMock.Object);
+
+            //act
+            CreateOrderResponseModel result = await service.CreateOrderAsync(order);
+
+            //assert
+            Assert.True(result.Type == OrderResponseType.Success);
+        }
+
+        private List<OrderDto> GetTestOrders()
+        {
+            List<OrderDto> orders = new List<OrderDto>
+            {
+                new OrderDto
+                {
+                    Id = 1,
+                    Status = "Waiting"
+                },
+
+                new OrderDto
+                {
+                    Id = 2,
+                    Status = "Coocking"
+                }
+            };
+
+            return orders;
+        }
+    }
+}
